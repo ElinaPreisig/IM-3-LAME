c23790e262ed02ccb0e35a8e242940ca0b908db5
Dinovinos <146704628+Dinovinos@users.noreply.github.com>
2023-10-09 15:38:08 +0200

    verknüpfung

--- js/script_memory.js
+++ js/script_memory.js
@@ -1,34 +1,59 @@
-const images = ["img/bild1.jpg", "img/bild2.jpg", "img/bild3.jpg", "img/bild4.jpg", "img/bild5.jpg", "img/bild6.jpg", "img/bild7.jpg", "img/bild8.jpg", "img/bild9.jpg", "img/bild10.jpg", "img/bild1.jpg", "img/bild2.jpg", "img/bild3.jpg", "img/bild4.jpg", "img/bild5.jpg", "img/bild6.jpg", "img/bild7.jpg", "img/bild8.jpg", "img/bild9.jpg", "img/bild10.jpg"];
-let cards = [];
+const supabaseUrl = 'https://tenojoxlyquvqackgeif.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlbm9qb3hseXF1dnFhY2tnZWlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYzMTY3NzAsImV4cCI6MjAxMTg5Mjc3MH0.4ZX9-F1GNCgWSmLleh5QLyDNkE1MljglPV54eemu-2w'
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+document.addEventListener("DOMContentLoaded", async function () {
+    try {
+        // Abrufen der Quiz-Daten aus Supabase
+        const { data, error } = await supabase.from('quizzes').select('*');
+
+        if (error) {
+            console.error('Fehler beim Abrufen von Quiz-Daten aus Supabase:', error);
+        } else {
+            const quizData = data[0]?.quizData || [];
+            console.log('Quiz-Daten aus Supabase:', quizData);
+
+            // Karten erstellen und anzeigen
+            const gameContainer = document.getElementById("game-container");
+            const cards = createCards();
+            cards.forEach(card => gameContainer.appendChild(card));
+        }
+    } catch (error) {
+        console.error('Ein unerwarteter Fehler ist aufgetreten:', error);
+    }
+});
+
+function createCards() {
+    const images = ["img/bild1.jpg", "img/bild2.jpg", "img/bild3.jpg", "img/bild4.jpg", "img/bild5.jpg", "img/bild6.jpg", "img/bild7.jpg", "img/bild8.jpg", "img/bild9.jpg", "img/bild10.jpg", "img/bild1.jpg", "img/bild2.jpg", "img/bild3.jpg", "img/bild4.jpg", "img/bild5.jpg", "img/bild6.jpg", "img/bild7.jpg", "img/bild8.jpg", "img/bild9.jpg", "img/bild10.jpg"];
+    const cards = [];
+
+    // Shuffle the images
+    images.sort(() => Math.random() - 0.5);
+
+    // Create card elements
+    for (let i = 0; i < images.length; i++) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.dataset.image = images[i];
+        card.dataset.index = i;
+
+        // Set the back image for all cards
+        card.style.backgroundImage = 'url("card-back.jpg")'; // Passe dies entsprechend an deinem Dateipfad an
+        card.innerText = "";
+
+        card.addEventListener("click", flipCard);
+        cards.push(card);
+    }
+
+    return cards;
+}
+
 let flippedCards = [];
 let matchedPairs = 0;
 let pairsFound = 0;
 let isCardFlipped = false;
 let timerInterval;
 let seconds = 0;
-
-const gameContainer = document.getElementById("game-container");
-const message = document.getElementById("message");
-const timer = document.getElementById("timer");
-
-// Shuffle the images
-images.sort(() => Math.random() - 0.5);
-
-// Create card elements
-for (let i = 0; i < images.length; i++) {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.dataset.image = images[i];
-    card.dataset.index = i;
-
-    // Set the back image for all cards
-    card.style.backgroundImage = `url(card-back.jpg)`;
-    card.innerText = "";
-
-    card.addEventListener("click", flipCard);
-    gameContainer.appendChild(card);
-    cards.push(card);
-}
 
 // Function to flip a card
 function flipCard() {
@@ -65,8 +90,8 @@ function checkMatch() {
             showMessage(`Gratulation! Du hast es geschafft! Zeit: ${seconds} Sekunden`);
         }
     } else {
-        card1.style.backgroundImage = `url(card-back.jpg)`;
-        card2.style.backgroundImage = `url(card-back.jpg)`;
+        card1.style.backgroundImage = 'url("card-back.jpg")';
+        card2.style.backgroundImage = 'url("card-back.jpg")';
         card1.innerText = "";
         card2.innerText = "";
     }
@@ -87,13 +112,50 @@ function showMessage(messageText) {
 }
 
 // Function to show a question in a pop-up
-function showQuestion() {
-    const questionText = "Gibt es den Studiengang Multimedia Production?";
-    const correctAnswer = "Ja";
+async function showQuestion() {
+    try {
+        // Annahme: Die Quiz-Fragen sind in Supabase gespeichert
+        const { data, error } = await supabase.from('quizzes').select('*');
 
-    const answer = prompt(questionText);
+        if (error) {
+            console.error('Fehler beim Abrufen von Quiz-Daten aus Supabase:', error);
+            return;
+        }
 
-    if (answer && answer.toLowerCase() === correctAnswer.toLowerCase()) {
+        const quizData = data[0]?.quizData || [];
+        const randomQuestion = quizData[Math.floor(Math.random() * quizData.length)];
+
+        const questionContainer = document.getElementById("question-container");
+        questionContainer.innerHTML = `
+            <div class="message-box">
+                <div class="message-content">
+                    <p>${randomQuestion.question}</p>
+                    <button id="answer-yes">Ja</button>
+                    <button id="answer-no">Nein</button>
+                </div>
+            </div>
+        `;
+
+        const answerYesButton = document.getElementById("answer-yes");
+        const answerNoButton = document.getElementById("answer-no");
+
+        answerYesButton.addEventListener("click", function () {
+            handleAnswer(randomQuestion, "Ja");
+        });
+
+        answerNoButton.addEventListener("click", function () {
+            handleAnswer(randomQuestion, "Nein");
+        });
+    } catch (error) {
+        console.error('Ein unerwarteter Fehler ist aufgetreten:', error);
+    }
+}
+
+// Function to handle user's answer
+function handleAnswer(question, userAnswer) {
+    const correctAnswer = question.answer;
+
+    if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
         // Richtige Antwort: Zeit abziehen (minus 15 Sekunden)
         seconds -= 15;
         if (seconds < 0) {
@@ -106,6 +168,12 @@ function showQuestion() {
 
     // Aktualisierte Zeit anzeigen
     timer.textContent = `Zeit: ${seconds} Sekunden`;
+
+    // Fragen-Container leeren
+    const questionContainer = document.getElementById("question-container");
+    questionContainer.innerHTML = "";
+
+    // Weitere Logik hier, wenn benötigt
 }
 
 // Start the timer
--- js/script_quiz_erstellen.js
+++ js/script_quiz_erstellen.js
@@ -1,3 +1,7 @@
+const supabaseUrl = 'https://tenojoxlyquvqackgeif.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlbm9qb3hseXF1dnFhY2tnZWlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYzMTY3NzAsImV4cCI6MjAxMTg5Mjc3MH0.4ZX9-F1GNCgWSmLleh5QLyDNkE1MljglPV54eemu-2w'
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 document.addEventListener("DOMContentLoaded", function () {
     const toggleSwitches = document.querySelectorAll('input[type="checkbox"]');
     
@@ -16,8 +20,26 @@ document.addEventListener("DOMContentLoaded", function () {
     const modal = document.querySelector('.modal');
     const playButton = document.querySelector('.play-button');
 
-    confirmButton.addEventListener('click', function () {
-        modal.style.display = 'flex';
+    confirmButton.addEventListener('click', async function () {
+        // Annahme: Alle Fragen sind in HTML-Elementen mit der Klasse "question" enthalten
+        const questions = document.querySelectorAll('.question input');
+        const quizData = [];
+
+        questions.forEach(question => {
+            const questionText = question.value;
+            const answerCheckbox = question.closest('.question').querySelector('input[type="checkbox"]');
+            const answer = answerCheckbox.checked ? "Ja" : "Nein";
+
+            quizData.push({ question: questionText, answer: answer });
+        });
+
+        if (error) {
+            console.error('Fehler beim Speichern in Supabase:', error);
+        } else {
+            console.log('Quiz erfolgreich in Supabase gespeichert:', data);
+            // Nachdem das Quiz erfolgreich gespeichert wurde, leite zum Memory-Spiel weiter
+            window.location.href = 'memory.html';
+        }
     });
 
     playButton.addEventListener('click', function () {
