b16d14b4aa11166ad43da83d5dbce6946906bc0d
nicoleschenk <146704768+nicoleschenk@users.noreply.github.com>
2023-10-16 10:34:16 +0200

    memory / button link

--- js/script_memory.js
+++ js/script_memory.js
@@ -1,6 +1,5 @@
-const supabaseUrl = 'https://tenojoxlyquvqackgeif.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlbm9qb3hseXF1dnFhY2tnZWlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYzMTY3NzAsImV4cCI6MjAxMTg5Mjc3MH0.4ZX9-F1GNCgWSmLleh5QLyDNkE1MljglPV54eemu-2w'
-const supabase = createClient(supabaseUrl, supabaseKey);
+import { supa } from "../supabase.js";
+
 
 document.addEventListener("DOMContentLoaded", async function () {
     try {
