112c1f0f46fb1331ef7ffea819b2de703496e933
nicoleschenk <146704768+nicoleschenk@users.noreply.github.com>
2023-11-06 20:16:33 +0100

    div ahpasst mit coaching

--- js/script_memory.js
+++ js/script_memory.js
@@ -228,6 +228,8 @@ async function showQuestion() {
 
     console.log('In saveGameData:', spielZeit);
 
+    
+
     // Hier wird die Zeit und der Spielername in die Datenbank gespeichert
     // const supa = supa.createClient('https://tenojoxlyquvqackgeif.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlbm9qb3hseXF1dnFhY2tnZWlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYzMTY3NzAsImV4cCI6MjAxMTg5Mjc3MH0.4ZX9-F1GNCgWSmLleh5QLyDNkE1MljglPV54eemu-2w');
 
