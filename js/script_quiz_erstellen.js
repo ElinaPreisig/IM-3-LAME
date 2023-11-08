fc53e51d2db30275049841586ec03e27e7ea7336
Elina <elina.preisig@stud.fhgr.ch>
2023-11-08 23:01:39 +0100

    id für memory

--- js/script_quiz_erstellen.js
+++ js/script_quiz_erstellen.js
@@ -2,9 +2,7 @@ import { supa } from "../supabase.js";
 console.log("Initialisierung abgeschlossen");
 
 
-/Function für Quizname/
-/* Passwortgenerator*/
-
+//Function für Passwortgenerator
 async function generateRandomNumericPassword(length) {
     const charset = "0123456789";
     let password = "";
@@ -15,11 +13,13 @@ async function generateRandomNumericPassword(length) {
     return password;
 }
 
+//Function für insert Daten Quiz in supabase
 async function insertQuizname() {
     const insertPromises = [];
     const name_quiz = document.querySelector('#name_quiz');
     const passwortDiv = document.querySelector('.modal-body p');
 
+    // Datum
     const getCurrentDate = () => {
         const now = new Date();
         const day = String(now.getDate()).padStart(2, '0');
@@ -28,6 +28,7 @@ async function insertQuizname() {
         return `${year}.${month}.${day}`;
     };
 
+    //einfügen in supabase
     const generatedPassword = await generateRandomNumericPassword(5);
     const { data, error} = await supa.from('Quiz').insert([
         {
@@ -46,33 +47,7 @@ async function insertQuizname() {
     }
 }
 
-/*
-
-funktionierender Code für eine Frage:
-async function insertFrage1() {
-  const frage1 = document.querySelector('#frage1');
-  const { data } = await supa.from("Fragen").insert(
-      [
-          {
-              fragesatz: frage1.value,
-          }
-      ]
-  );
- 
- if (data) {
-    console.log('Entry was created successfully', data);
-  } else {
-    console.log('Error occured')
-  }
-}
-
-*/
-/* Function für Fragen*/
-async function getID() {
-    const quizId = await supa.from("Quiz").select("id").eq("name", document.querySelector('#name_quiz').value);
-    console.log(quizId);
-}
-
+//Function für insert Daten Fragen in supabase
 async function insertFragen() {
     // Define the arrays outside of the loop
     const fragenIDs = ['#frage1', '#frage2', '#frage3', '#frage4', '#frage5'];
@@ -90,6 +65,7 @@ async function insertFragen() {
 
     const quizId = data[0].id; // Assuming quiz_id is in the first element
 
+    //Schleife für die Fragen
     for (let i = 0; i < 5; i++) {
 
         const fragenElement = document.querySelector(fragenIDs[i]);
@@ -110,6 +86,7 @@ async function insertFragen() {
             console.log("Slider inaktiv");
         }
 
+        //einfügen in supabase
         const {data, error} = await supa.from('Fragen').insert(
             [
                 {
@@ -130,30 +107,6 @@ async function insertFragen() {
 
     return results; // Return results if needed for further processing
 }
-
-
-    /*for (let i = 0; i <= 5; i++) {
-
-        const frage = document.querySelector(fragen[i]);
-
-        
-        const { data } = await supa.from("Fragen").insert([
-            {
-                fragesatz: frage.value,
-            }
-        ]);
-    }
-*/
-/*
-    if (data) {
-        console.log('Frage' + [i] + 'wurde erfolgreich eingefügt');
-    } else {
-        console.log('Fehler beim Einfügen von Frage');
-    }
-    */
-
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
     const toggleSwitches = document.querySelectorAll('input[type="checkbox"]');
@@ -177,9 +130,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
     confirmButton.addEventListener('click', function () {
         modal.style.display = 'flex';
+        memoryURL();
     });
+    
+   async function memoryURL() {
+        const { data , error } = await supa.from('Quiz').select('id').eq('name', document.querySelector('#name_quiz').value);
+
+        if (error) {
+            console.error('Error fetching quiz_id from other table', error);
+            return;
+        }
+    
+        console.log('Quiz ID:', data[0].id);
+        const quizId = data[0].id; // Assuming quiz_id is in the first element
+    
+        const playURL = document.querySelector('.game-id-url');
+        playURL.href = "memory.html" + `?spiel=${quizId}`;
+        console.log(playURL.href);
+        console.log(playURL);
+    }
 
     playButton.addEventListener('click', function () {
         modal.style.display = 'none';
+        console.log("memoryURL ausgeführt");
     });
 });
