08968295ae1a35b10e3398405bc1730a8a0f9b78
nicoleschenk <146704768+nicoleschenk@users.noreply.github.com>
2023-11-08 16:23:07 +0100

    Update script_memory.js

--- js/script_memory.js
+++ js/script_memory.js
@@ -256,7 +256,7 @@ async function letzteFrage() {
     letzteFrage();
 
     console.log('In saveGameData:', spielZeit);
-}
+
 
     // Hier wird die Zeit und der Spielername in die Datenbank gespeichert
     // const supa = supa.createClient('https://tenojoxlyquvqackgeif.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlbm9qb3hseXF1dnFhY2tnZWlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYzMTY3NzAsImV4cCI6MjAxMTg5Mjc3MH0.4ZX9-F1GNCgWSmLleh5QLyDNkE1MljglPV54eemu-2w');
@@ -276,7 +276,7 @@ async function letzteFrage() {
         .catch((error) => {
             console.error('Fehler beim Speichern der Spielzeit und des Spielername:', error);
         });
-
+}
 
     //     // Function to display the save confirmation pop-up
     // function displaySaveConfirmationPopup() {
