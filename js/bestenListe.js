8d00c14d3a1b7e401dee76699b6611aa84e0502d
nicoleschenk <146704768+nicoleschenk@users.noreply.github.com>
2023-11-12 16:13:40 +0100

    bestenliste

--- js/bestenListe.js
+++ js/bestenListe.js
@@ -6,37 +6,44 @@ function formatTime(seconds) {
     return `${minutes} Minuten ${remainingSeconds} Sekunden`;
 }
 
-async function showTimes(){
-    const { data, error } = await supa
-        .from('Spielzeit')
-        .select('name_user, spielzeit')
-        .order('spielzeit', { ascending: true })
-        .limit(5);
-    console.log(data);
-    const bestenlisteTable = document.getElementById('bestenliste-table');
-    const tbody = bestenlisteTable.querySelector('tbody');
-
-    data.forEach((eintrag, index) => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td>${eintrag.name_user}</td>
-            <td>${formatTime(eintrag.spielzeit)}</td>
-        `;
-        tbody.appendChild(row);
-    });
+async function showTimes() {
+    try {
+        const { data, error } = await supa
+            .from('Spielzeit')
+            .select('name_user, spielzeit')
+            .range(0, 4); // Wähle die ersten 5 Einträge (0-4, da die Indexierung bei 0 beginnt)
 
 
 
+        if (error) {
+            console.error('Fehler beim Abrufen der Daten:', error.message);
+            return;
+        }
+
+        console.log(data);
+
+        data.sort((a, b) => parseInt(a.spielzeit) - parseInt(b.spielzeit));
 
 
-    if (error) {
-        console.error('Fehler beim Abrufen der Daten:', error);
-        return;
-    }}
+        const bestenlisteTable = document.getElementById('bestenliste-table');
+        const tbody = bestenlisteTable.querySelector('tbody');
+
+        tbody.innerHTML = '';
+
+        data.forEach((eintrag, index) => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${eintrag.name_user}</td>
+                <td>${formatTime(parseInt(eintrag.spielzeit))}</td>
+            `;
+            tbody.appendChild(row);
+        });
+    } catch (error) {
+        console.error('Fehler beim Abrufen der Daten:', error.message);
+    }
+}
+
 showTimes();
-
-    // Daten in die Tabelle einfügen
-
 
 
 function playSoundEffect() {
